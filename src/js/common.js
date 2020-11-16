1105f6eeddd7a5f6af6d0b55f2655dfe15c8dcb4
HorbynkoIvan <horbynko.i@gmail.com>
2020-11-16 15:12:48 +0200

    added counter

--- src/js/common.js
+++ src/js/common.js
@@ -14,14 +14,15 @@ import {plyrPlayer} from './partials/plyr.js'
 window.addEventListener('DOMContentLoaded', () => {
     openMore();
     submitForm();
+    initializeTimer();
 
     function openMore() {
         const $btnMore = $('#btn_more');
         const $shadow = $('.about_top .shadow');
         const $aboutBottom = $('.about_bottom');
 
-        $btnMore.on('click', function (){
-            $(this).text(function(i, text){
+        $btnMore.on('click', function () {
+            $(this).text(function (i, text) {
                 return text === "Читати повністю" ? "Згорнути" : "Читати повністю";
             })
 
@@ -30,7 +31,7 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     }
 
-    function submitForm(){
+    function submitForm() {
         $(document).on('submit', '#form', function (event) {
             event.preventDefault();
             var emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -77,58 +78,24 @@ window.addEventListener('DOMContentLoaded', () => {
 
         });
     }
-})
 
-// $(document).ready(function () {
-//     $('.js_bf_parf_seo').addClass('bf_parf_seo--hide');
-//     $('.js_button_btn_show').on('click', function () {
-//         $(this).closest('.js_bf_parf_seo').removeClass('bf_parf_seo--hide');
-//         $(this).closest('.js_bf_parf_seo_btn_shadow').hide();
-//     });
-//
-//     $(document).on('submit', '#form', function (event) {
-//         event.preventDefault();
-//         var emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//         var email = $(this).serializeArray()[0].value;
-//
-//         if (email.length) {
-//             if (!emailRegExp.test(email)) {
-//                 email.classList.add('error');
-//                 $('.js-subscription_tooltip').show();
-//                 return false;
-//             }
-//
-//             $.post(window.location.pathname, {email: email, id: 30})
-//                 .done(function (response) {
-//                     $('.bf_parf_form_wrap').html('');
-//                     $('.bf_parf_form_wrap').html(' <div class="second_screen js_form_success">\n' +
-//                         '                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-//                         '                            <circle cx="24" cy="24" r="22.5" stroke="#00FFF0" stroke-width="3"/>\n' +
-//                         '                            <path d="M15 23.0963L23.0125 31.1088L35 19.1213" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-//                         '                        </svg>\n' +
-//                         '\n' +
-//                         '                        <div>Вы подписаны</div>\n' +
-//                         '                    </div>');
-//
-//                     dataLayer.push({
-//                         'eventCategory': 'landing',
-//                         'eventAction': 'blackFriday',
-//                         'eventLabel': 'formSend',
-//                         'event': 'autoEvent'
-//                     })
-//                 })
-//                 .fail(function (data) {
-//                     $('.bf_parf_form_wrap').html('');
-//                     $('.bf_parf_form_wrap').html(' <div class="second_screen js_form_success">\n' +
-//                         '                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
-//                         '                            <circle cx="24" cy="24" r="22.5" stroke="#00FFF0" stroke-width="3"/>\n' +
-//                         '                            <path d="M15 23.0963L23.0125 31.1088L35 19.1213" stroke="white" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>\n' +
-//                         '                        </svg>\n' +
-//                         '\n' +
-//                         '                        <div>Вы уже подписаны</div>\n' +
-//                         '                    </div>');
-//                 });
-//         }
-//
-//     });
-// });
+    function setTimePage(e, t, n, o) {
+        const i = document.getElementById("days"),
+            r = document.getElementById("hours"),
+            s = document.getElementById("minutes"),
+            a = document.getElementById("seconds");
+        i.innerHTML = e,
+            r.innerHTML = t,
+            s.innerHTML = n,
+            a.innerHTML = o
+    }
+
+    function initializeTimer() {
+        var e, t, n, o = (new Date(2020, 10, 17) - new Date) / 1e3;
+        0 < o ?
+            (n = (t = (e = o / 60) / 60) / 24, e = 60 * (t - Math.floor(t)), n = Math.floor(n), t = Math.floor(t) - 24 * n, o = Math.floor(60 * (e - Math.floor(e))), e = Math.floor(e), setTimePage(n, t, e, o), timerId = setInterval(function () {
+            0 == o ? 0 == e ? 0 == t ? 0 == n ? showMessage(timerId) : (n--, t = 24, o = e = 59) : (t--, o = e = 59) : (e--, o = 59) : o--, setTimePage(n, t, e, o)
+        }, 1e3)) :
+            $("#timer").hide()
+    }
+})
